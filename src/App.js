c6217aee168433a475247712a976b601c89f4de9
jdyj <jdyj@naver.com>
2022-05-07 06:45:45 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Switch, Route, Redirect } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Switch>
+      </Switch>
+    </>
+  );
+};
+
+export default App;
---
+++ src/remote/index.js
@@ -0,0 +1,10 @@
+import * as MemberApi from "./api/memberApi";
+
+import urls from "./urls";
+import httpClient from "./httpClient";
+
+export {
+  MemberApi,
+  urls,
+  httpClient,
+};
