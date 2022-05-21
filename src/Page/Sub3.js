e4feabb9b43ddcfd7a3585e3e9806c12e1ef049f
jdyj <jdyj@naver.com>
2022-05-21 22:20:28 +0900

    모달

--- src/Page/Sub3.js
+++ src/Page/Sub3.js
@@ -55,12 +55,92 @@ const FooterContainer = styled.div`
   margin-top: 3.813rem;
   margin-bottom: 2rem;
 `;
+
+const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0,0,0,0.50);
+    z-index: 0;
+`;
+
+const ModalContainer = styled.div`
+    position: fixed;
+    transform: translate(-50%, -50%);
+    width: 19.75rem;
+    height: 11.875rem;
+    padding: 50px 67px;
+    border: solid 3px #000;
+    background-color: #fff;
+    border-radius: 90px;
+    top: 54%;
+    left: 50%;
+    text-align: center;
+
+`;
+
+const ModalComment = styled.div`
+    width: 11.375rem;
+    height: 1.875rem;
+    margin: 0 0 0;
+    font-family: Montserrat_SemiBold;
+    font-size: 15px;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 2;
+    letter-spacing: -0.75px;
+    text-align: left;
+    color: #000;
+`
+
+const Wrapper = styled.div`
+    display: flex;
+`
+const OkButton = styled.div`
+    width: 1.75rem;
+    height: 1.875rem;
+    margin: 30px 126px 0 0;
+    font-family: Montserrat;
+    font-size: 15px;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 2;
+    letter-spacing: -0.75px;
+    text-align: left;
+    color: #000;
+`
+
+const NoButton = styled.div`
+    width: 1.5625rem;
+    height: 1.875rem;
+    margin: 30px 3px 0 0;
+    font-family: Montserrat;
+    font-size: 15px;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 2;
+    letter-spacing: -0.75px;
+    text-align: left;
+    color: #000;
+`
+
 const letterList = [mezin1, mezin2, mezin3, mezin4, mezin5];
 const index = () => {
   const context = useContext(Context);
   const { paper } = context;
   const [letter, setLetter] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
   const history = useHistory();
+
+  const modalToggle = () => {
+      setModalOpen(!modalOpen);
+  }
+
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -88,8 +168,21 @@ const index = () => {
       </LetterContainer>
 
       <FooterContainer>
-        <Footer title={"NEXT"} onClick={didTapNext} />
+        <Footer title={"NEXT"} onClick={modalToggle} />
       </FooterContainer>
+      {modalOpen ?
+        <Background>
+            <ModalContainer>
+                <ModalComment>
+                    편지작성을 완료하시겠습니까?
+                </ModalComment>
+                <Wrapper>
+                <OkButton onClick={didTapNext}>YES</OkButton>
+                <NoButton onClick={modalToggle}>NO</NoButton>
+                </Wrapper>
+            </ModalContainer>
+        </Background> : null
+      }
     </>
   );
 };
